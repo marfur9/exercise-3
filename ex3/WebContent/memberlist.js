7b1ec79c9c28ab1b8a983b8b31d3378fa47e4069
Magnar Gya <magnar.gya@hotmail.com>
2016-09-12 13:44:55 +0200

    Kopierte my shit over, yo

---
+++ ex3/WebContent/memberlist.js
@@ -0,0 +1,34 @@
+/**
+ * haha, this is for the hecking code stoopid
+ */
+"use strict";
+
+function init(){
+	let table = document.getElementById("memberTable");
+	let button = document.getElementById("addMember");
+	
+	button.addEventListener("click",addMember);
+	setInterval(getTable,10000);
+}
+
+function getTable(){
+	
+}
+
+function updateMembers(){
+	
+}
+
+function deleteMember(member){
+	
+}
+
+function editMember(member){
+	
+}
+
+function addMember(){
+	
+}
+
+window.addEventListener("load",init,true)
