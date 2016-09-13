64b2d64b94883d84c50c2e99a2112200a7017fa9
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-13 16:57:59 +0200

    I did an ajax. It's probably super wrong, but it's a framework for something totally right.

--- ex3/WebContent/WEB-INF/AjaxComm.js
+++ ex3/WebContent/WEB-INF/AjaxComm.js
@@ -1,3 +1,26 @@
 /**
  * 
  */
+
+"use strict";
+  
+function onLoad() {
+	getUpdates(-1);
+}
+  
+function viewMessage(m) {
+	var parts = m.split(" ");
+	for (var i = 0; i < parts.length%5; i++) {
+		var j = 5*i;
+		updateMembers(parts[j+0],parts[j+1],parts[j+2],parts[j+3],parts[j+4])
+	}
+}
+
+function getUpdates(e) {
+	let url = config.servicesPath + "/updates"
+    const ajax = new AJAXConnection(url)
+    ajax.onsuccess = viewMessage
+    ajax.get(-1)
+}
+    
+window.addEventListener("DOMContentLoaded",onLoad,true)
