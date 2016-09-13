2d02783fc4b425ff926182302680c077008b661e
Magnar Gya <magnar.gya@hotmail.com>
2016-09-13 16:49:36 +0200

    Made update and stuff

--- ex3/WebContent/WEB-INF/memberlist.js
+++ ex3/WebContent/WEB-INF/memberlist.js
@@ -28,15 +28,56 @@ function getTable(){
 	//end for something
 }
 
-function updateMembers(){
+function updateMembers(idn, fname, lname, address, phone){
+	let table = document.getElementById("membertable");
+	let row = table.getElementById("r"+idn);
+	
+	if(row!=null){
+		let fnameCell = row.getElementById("fnameCell");
+		fnameCell.textContent = fname;
+		let lnameCell = row.getElementById("lnameCell");
+		lnameCell.textContent = lname;
+		let addressCell = row.getElementById("addressCell");
+		addressCell.textContent = address;
+		let phoneCell = row.getElementById("phoneCell");
+		phoneCell.textContent = phone;
+	}else{
+		row = table.insertRow();
+		row.id = ("r"+idn);
+		
+		let fnameCell = row.insertCell(0);
+		fnameCell.id = "fnameCell";
+		fNameCell.textContent = fname;
+		
+		let lnameCell = row.insertCell(1);
+		lnameCell.id = "lnameCell";
+		fNameCell.textContent = lname;
+		
+		let addressCell = row.insertCell(2);
+		addressCell.id = "addressCell";
+		addressCell.textContent = address;
+		
+		let phoneCell = row.insertCell(3);
+		phoneCell.id = "phoneCell";
+		phoneCell.textContent = phone;
+		
+		let editButton = document.createElement("input");
+		editButton.setAttribute("type", "button");
+		
+		let deleteButton = document.createElement("input");
+		deleteButton.setAttribute("type", "button");
+		
+		editButton.addEventListener("click", editMember);
+		deleteButton.addEventListener("click", deleteMember);
+		
+	}
+}
+
+function deleteMember(id){
 	
 }
 
-function deleteMember(member){
-	
-}
-
-function editMember(member){
+function editMember(id){
 //	let newFName = prompt("Enter firstname", member.fName);
 //	let newLName = prompt("Enter lastname", member.lName);
 //	let newAddress = prompt("Enter address", member.address);
