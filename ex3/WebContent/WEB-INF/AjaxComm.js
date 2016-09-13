f96d6ddb1d51cfd794c40cfcb92473947d1af8be
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-13 16:59:30 +0200

    wat

--- ex3/WebContent/WEB-INF/AjaxComm.js
+++ ex3/WebContent/WEB-INF/AjaxComm.js
@@ -12,7 +12,7 @@ function viewMessage(m) {
 	var parts = m.split(" ");
 	for (var i = 0; i < parts.length%5; i++) {
 		var j = 5*i;
-		updateMembers(parts[j+0],parts[j+1],parts[j+2],parts[j+3],parts[j+4])
+		updateMember(parts[j+0],parts[j+1],parts[j+2],parts[j+3],parts[j+4])
 	}
 }
 
