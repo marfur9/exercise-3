b234906e3467afb3c020ffcb996b46f1fb50402a
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-15 15:28:38 +0200

    Fixed import of json data on update, and pushing json objects of members

--- ex3/WebContent/script/AjaxComm.js
+++ ex3/WebContent/script/AjaxComm.js
@@ -11,14 +11,10 @@ function onLoad() {
 }
   
 function viewMessage(m) {
-	var parts = m.split(" ");
-	for (var i = 0; i < parts.length%5; i++) {
-		var j = 5*i;
-		var jsontext = 	"{id:" + parts[j+0] +
-						",fname:" + parts[j+1] +
-						",lname:" + parts[j+2] +
-						",address:" + parts[j+3] +
-						",phone:" + parts[j+4] + '}';
+	var data = JSON.parse(m);
+	
+	for (var i = 0; i < data.newMembers.length; i++) {
+		var jsontext = JSON.stringify(data.newMembers[i])
 		updateMember(jsontext)
 	}
 }
