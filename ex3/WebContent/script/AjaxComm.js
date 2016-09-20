0b7c894c2b5ca97dbe289dbcc55406805672bd40
ToreMartin <ToreMartin@Tore-Laptop>
2016-09-20 13:14:05 +0200

    Did the missing functions for the Ajax communication

--- ex3/WebContent/script/AjaxComm.js
+++ ex3/WebContent/script/AjaxComm.js
@@ -9,8 +9,9 @@ let memberId = null;
 function onLoad() {
 	getUpdates(-1);
 }
-  
-function viewMessage(m) {
+
+//Passes returned members to the memberlist
+function passMember(m) {
 	var data = JSON.parse(m);
 	console.log("Members found: " + data.updates.newMembers.length)
 	for (var i = 0; i < data.updates.newMembers.length; i++) {
@@ -24,8 +25,34 @@ function getUpdates(e) {
 	memberId = e
 	let url = "../Mservices/data/updates/"+memberId
     const ajax = new AJAXConnection(url)
-    ajax.onsuccess = viewMessage
+    ajax.onsuccess = passMember
     ajax.get()
 }
+
+function postMember(m) {
+	let url = "../Mservices/data/member/"
+    const ajax = new AJAXConnection(url)
+    ajax.onsuccess = passMember
+    ajax.post(m)
+}
+
+function putMember(m) {
+	member = JSON.parse(m);
+	memberId = member.memberId
+	let url = "../Mservices/data/member/"+memberId
+    const ajax = new AJAXConnection(url)
+    ajax.onsuccess = passMember
+    ajax.put(m)
+}
+
+function deleteMember(m) {
+	member = JSON.parse(m);
+	memberId = member.memberId
+	let url = "../Mservices/data/member/"+memberId
+    const ajax = new AJAXConnection(url)
+    ajax.onsuccess = passMember
+    ajax.del()
+}
     
+
 window.addEventListener("DOMContentLoaded",onLoad,true)
